997d289dd4a338e8e6662afb7d54d91a29b39ff9
Emmanuel Oreoluwa Sanyaolu <emmasanyaolu1@gmail.com>
2026-05-27 15:54:20 -0300

    I designed and implemented the LearningDam login page to match the existing home and register UI style, updated the login form so it only includes email and password, improved the register experience by styling the user already exists state as a clear red error message, and fixed the registration success redirect by using the proper navigation handler.

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const {pool} = require(`${__dirname}/../db/db.js`)
 
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 async function generatePassword(plainPassword){
     let hashedPassword = await bcrypt.hash(plainPassword, 10)
@@ -18,10 +18,20 @@ async function comparePassword(plainPassword, hashPassword){
 
 async function createUser(name, email, role, hashedPassword){
     const result  = await pool.query('INSERT INTO users (email, password, role, first_name, last_name) VALUES($1, $2, $3, $4, $5) RETURNING *', [email, hashedPassword, role, name, name]);
+
+    return result.row;
 }
 
 async function checkIfUserExists(email){
     const userExists = await pool.query('SELECT * FROM users where email = $1', [email])
+    
+    // For Debugging
+    // console.log(userExists)
+    // console.log(userExists.rowCount)
+
+    if(userExists.rowCount == 1){
+        return true;
+    }
 }
 
 module.exports = {generatePassword, comparePassword, checkIfUserExists, createUser}
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = require('express').Router()
 const {generatePassword, comparePassword, checkIfUserExists, createUser} = require(`${__dirname}/../controllers/authController`)
+
+
 router.route('/register')
     .get((req, res) => {
         res.send('Send HTML')
@@ -11,26 +13,58 @@ router.route('/register')
         const role = req.body.role;
         const password = req.body.password;
         const phone_number = req.body.phone_number
-        if (await checkIfUserExists(email)){
+
+        // Check if user exists
+        let userExists = await checkIfUserExists(email);
+        if (userExists){
+            res.statusCode = 409
             res.json({
                 message : "User Already Exists"
             })
         }
         else{
             let hashedPassword = await generatePassword(password);
-            await createUser(name, email, role, hashedPassword)
+            const userObj = await createUser(name, email, role, hashedPassword)
+            req.session.user = {
+                name : name, 
+                email : email,
+                role : role,
+                phone_number : phone_number
+            }
+            req.session.loggedIn = true;
+
+            res.statusCode = 200;
+            res.json({
+                message : "Account Created"
+            })
         }
-
-        
-
     })
 
 router.route('/login')
-    .get((res, req) => {
+    .get((req, res) => {
         res.send('Send Login Form')
     })
-    .post((res, req) => {
-        res.send("Authorize")
+    .post((req, res) => {
+        let email = req.body.email
+    })
+
+    // Check session
+router.route('/me')
+    .get((req, res) => {
+        if(req.session.loggedIn){
+            res.json(
+                {
+                    loggedIn: true,
+                    userObj: req.session.user
+                }
+            )
+        }
+
+        else{
+            res.json(
+                {loggedIn : false}
+            )
+        }
     })
 
 module.exports = router
